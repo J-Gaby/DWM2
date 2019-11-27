df5e2e0a3f2fdff2cab0bb4f551021eec49d6113
J-Gaby <gabriel.desmaris@gmail.com>
2019-11-27 10:24:00 +0000

    css php

--- header.php
+++ header.php
@@ -3,4 +3,24 @@
 <head>
     <meta charset="utf-8">
     <title>Phpwebsite</title>
+    <link rel="stylesheet" type="text/css" href="header.css">
 </head>
+
+<header>
+	<nav>
+		<div>
+			<li><a href="index.php">Accueil</a></li>
+		</div>
+		<div>
+			<li>Autre page</li>
+		</div>
+		<div>
+			<li>666</li>
+		</div>
+		<div>
+			<li>Contact</li>
+		</div>
+	</nav>
+
+	<h1>Calculatrice</h1>
+</header>
